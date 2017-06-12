009a0c399e6fee8b75c1e174316e058e464f5907
jootan19 <joo.tan@me.com>
2017-06-12 18:05:26 +0800

    add tweet.js

---
+++ tweet.js
@@ -0,0 +1,12 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+
+//  tweet 'hello world!' 
+// 
+T.post('statuses/update', { status: 'Bot says: hello world!' }, function(err, data, response) {
+  console.log(data)
+})
+
+
