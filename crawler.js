37f91e3b4da85b423a80a288169070d2a49b1df1
jootan19 <joo.tan@me.com>
2017-06-15 19:17:47 +0800

    updates

--- crawler.js
+++ crawler.js
@@ -2,40 +2,43 @@
 var fs = require('fs');
 var sinceID = fs.readFileSync('sinceID.log', 'UTF-8'); // retrieve since_id for search params
 
-//twitter
+//Twitter
 var Twitter = require('twitter');
 var config = require('./config.js');
 var T = new Twitter(config);
 var tweetCount = 0;
 
-// Set up your search parameters
+// Set up search parameters
 var params = {
-  q: '#neuroscience' + 'attention',
+  q: '#trump',
   count: 100,
   result_type: 'recent',
   since_id: sinceID,
   lang: 'en'
 };
 
-// Initiate your search using the above paramaters
+// Initiate search using the above paramaters
 T.get('search/tweets', params, function(err, data) {
-  // If there is no error, proceed
+  
   fs.writeFile("sinceID.log", data.search_metadata.max_id, function(err) {
-    if(err) {
-        return console.log(err);
-      }
+    if(err) {return console.log(err);}
     console.log("since ID log was saved!");
   }); 
-
-console.log(data.search_metadata);
-
-var logger = fs.createWriteStream('log.txt', {
-  flags: 'a'
+  
+  // Display search meta data
+  console.log(data.search_metadata);
+  
+  // Open log file
+  var logger = fs.createWriteStream('log.txt', {
+    flags: 'a'
   });
-logger.write('Search query:' + data.search_metadata.query + '\n');
-
-var textlog = fs.createWriteStream('textlog.txt', {
-  flags: 'a'
+  
+  // Store search query
+  logger.write('Search query:' + data.search_metadata.query + '\n');
+  
+  // Open log file for unique tweets
+  var textlog = fs.createWriteStream('textlog.txt', {
+    flags: 'a'
   });
   
   if(!err){
@@ -44,23 +47,29 @@ var textlog = fs.createWriteStream('textlog.txt', {
       
       var tweet =  String(data.statuses[i].text);
       var rtchecktext = tweet.substring(0,2);
+      var logtext = '\n\n---'+ String(i) +'\nTweet: ' + tweet +   
+      '\nScreenName: ' + data.statuses[i].user.screen_name +
+      '\nCreated at: ' + data.statuses[i].created_at +
+      '\nID: ' + data.statuses[i].id_str;
+        
       if (rtchecktext !== 'RT'){
         tweetCount++;
         textlog.write(tweet + '\n');
+        console.log(logtext);
       }
-      var logtext = '\n\n---'+ String(i) +'\nTweet: ' + tweet +   
-        '\nScreenName: ' + data.statuses[i].user.screen_name +
-        '\nCreated at: ' + data.statuses[i].created_at +
-        '\nID: ' + data.statuses[i].id_str;
-      console.log(logtext);
+      
       logger.write(logtext);
     }
+    
     logger.write('Unique Tweets:' + tweetCount + '\n-------------\n');
     console.log('Unique Tweets:' + tweetCount);
   } else {
+    
     console.log(err);
   }
+  logger.end();
+  textlog.end();
+
 });
 
-logger.end();
-textlog.end();
+
